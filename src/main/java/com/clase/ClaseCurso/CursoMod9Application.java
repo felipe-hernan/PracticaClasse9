ba63935cba0c9e292ca62b82c79523e493532085
Felipe Burgos <hhh.hb38@gmail.com>
2022-06-02 21:23:56 -0300

    Comienzo del proyecto

---
+++ src/main/java/com/clase/ClaseCurso/CursoMod9Application.java
@@ -0,0 +1,13 @@
+package com.clase.ClaseCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMod9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMod9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/clase/ClaseCurso/CursoMod9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clase.ClaseCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMod9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
