194688db234a3834c7068fe982094392b4b09358
Felipe Burgos <hhh.hb38@gmail.com>
2022-06-03 15:44:43 -0300

    Comienzo

---
+++ src/main/java/com/clase/ClaseCurso/Cliente/Cliente.java
@@ -0,0 +1,40 @@
+package com.clase.ClaseCurso.Cliente;
+
+public class Cliente {
+
+    private Long id;
+    private String nombre;
+    private String apellido;
+
+    public Cliente(Long id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+}
---
+++ src/main/java/com/clase/ClaseCurso/Controller/HolaController.java
@@ -0,0 +1,47 @@
+
+package com.clase.ClaseCurso.Controller;
+
+import com.clase.ClaseCurso.Cliente.Cliente;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+    
+    @GetMapping("/hola")
+    public String decirHola(@RequestParam String nombre,
+                                            @RequestParam int edad,
+                                            @RequestParam String profesion){
+        return "Hello World. Tu nombre es: "+nombre+" Tu edad es: "+edad+" Tu profesion es: "+profesion;
+    }
+    
+    @PostMapping("/cliente")
+    public void nuevoCliente(@RequestBody Cliente cli){
+            System.out.println("Datos cliente: "+cli.getNombre()+" Apellido: "+cli.getApellido());
+    }
+    
+    @GetMapping("/cliente/traer")
+    @ResponseBody
+    public List<Cliente> traerClientes(){
+        List<Cliente> listaClientes = new ArrayList<>();
+        listaClientes.add(new Cliente(1L,"Dario","Posadas"));
+        listaClientes.add(new Cliente(2L,"Alberto","Rojas"));
+        listaClientes.add(new Cliente(3L,"Victor","Ruiz"));
+        listaClientes.add(new Cliente(4L,"Alejandro","Bautista"));
+        
+        return listaClientes;
+    }
+    @GetMapping("/pruebaresponse")
+    ResponseEntity<String> traerRespuestas(){
+        return new ResponseEntity<>("Esto es un mensaje Response Entity",HttpStatus.OK);
+    }
+    
+}
